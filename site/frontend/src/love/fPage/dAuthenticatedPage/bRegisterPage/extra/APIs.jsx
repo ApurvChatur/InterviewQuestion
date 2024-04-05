a3dd77bb1ffbe1fe08ec38486c7e1c2bfa2aebe3
ApurvChatur <chaturapurv@gmail.com>
2024-04-05 17:23:02 +0530

    Summer

--- site/frontend/src/love/fPage/dAuthenticatedPage/bRegisterPage/extra/APIs.jsx
+++ site/frontend/src/love/fPage/dAuthenticatedPage/bRegisterPage/extra/APIs.jsx
@@ -22,7 +22,7 @@ const APIs = {
         aDescription: "This is some detailed description",
         aStatus: true,
 
-        cRole: '65915580e476907a9789160e'
+        cRole: '660fda27b112ec1f2d15caf9'
       }
     })
     .then(response => {
